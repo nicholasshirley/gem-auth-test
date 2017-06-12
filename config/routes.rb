7a97aa476d1d4e4d14562354b19c336edefb5989
Nicholas Shirley <n.shirley1@gmail.com>
2017-06-12 21:14:27 +0200

    prefix all API requests and update rake task

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
 
   constraints subdomain: 'api' do
+    mount_devise_token_auth_for 'User', at: 'auth'
     resources :notes
   end
+
+  root 'app#index'
+  get '*path' => 'app#index', via: :all
 end
