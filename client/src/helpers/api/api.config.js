1f3f22893585019a4edf635a7feff1b161f3d792
VienNguyen113 <luongvien13t2@gmail.com>
2017-06-29 16:26:16 +0700

    clean helpers

--- client/src/helpers/api/api.config.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-import { getEnv } from 'config/app'
-
-const API_URL = getEnv()
--- client/src/helpers/api/index.js
+++
@@ -1,6 +0,0 @@
-import { fetchJson }             from './fetch.module'
-import { fetchJsonExternal }     from './fetch.external.module'
-import { getPacketData }         from './packet.data'
-import { getPacketFormData }     from './packet.formdata'
-
-export { fetchJson, fetchJsonExternal, getPacketData, getPacketFormData }
